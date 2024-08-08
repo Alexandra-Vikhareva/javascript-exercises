665f37c5dcaf9eb3630c8779a21a2b7ca31c99b3
Sasha <st087955@student.spbu.ru>
2024-08-08 15:37:44 +0300

    do the seventh task

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemperatute) {
+  result = (fahrenheitTemperatute - 32) * 5 / 9;
+  return Math.sign(result) * Math.round(Math.abs(result) * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemperature) {
+  result = celsiusTemperature * 9 / 5 + 32;
+  return Math.sign(result) * Math.round(Math.abs(result) * 10) / 10
 };
 
 // Do not edit below this line
