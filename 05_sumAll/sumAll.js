080766618fb4bfcaf2100c29e6b5862f942a2748
Sasha <st087955@student.spbu.ru>
2024-08-08 15:03:46 +0300

    do the fifth task

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(first_num, second_num) {
+    if ((typeof first_num != 'number' || first_num % 1 != 0 || first_num <= 0) 
+        || (typeof second_num != 'number' || second_num % 1 != 0 || second_num <= 0)) {
+            return 'ERROR'
+    }
+    else {
+        if (first_num > second_num) {
+            [first_num, second_num] = [second_num, first_num];
+        };
+        let result = 0;
+        for (let i = first_num; i <= second_num; i++) {
+            result += i;
+        };
+        return result
 
+    }
 };
 
 // Do not edit below this line
