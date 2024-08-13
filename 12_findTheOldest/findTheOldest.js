89a3dca3595ec6415becc0b5060928cd70d9f58b
Sasha <st087955@student.spbu.ru>
2024-08-13 17:42:01 +0300

    do the twelfth task

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    function getAge(person){
+        let age;
+        if (person.yearOfDeath) {
+            age = person.yearOfDeath - person.yearOfBirth;
+            console.log(age);
+        }
+        else { 
+            age = (new Date).getFullYear() - person.yearOfBirth;
+            console.log('sg')
+        }
+        return age
+    }
 
+    people.sort((a, b) =>  getAge(b) - getAge(a) );
+
+    return people[0]
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
